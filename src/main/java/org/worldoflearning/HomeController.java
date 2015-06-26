4daa8211cd9a4197499802db0beb097049a98896
Jamjamja <jan_henssler@yahoo.de>
2015-06-26 16:38:23 +0200

    CSS funktioniert jetzt
    So Leute habe das mit dem CSS gefixt.
    Der HomeController hat jetzt für das Impressum einen eigenes Mapping, da
    ich mir dachte es einfacher für das Verständnis als alles automatisch
    mit einem Controller zu mappen.

--- src/main/java/org/worldoflearning/HomeController.java
+++ src/main/java/org/worldoflearning/HomeController.java
@@ -1,23 +1,19 @@
 package org.worldoflearning;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HomeController {
 
 	@RequestMapping(value = "/")
 	public String home() {
-		return "index";
+		return "home/index";
 	}
-
-	@RequestMapping(method = RequestMethod.GET)
-	public Model navigation(
-			@RequestParam(value = "link", required = true, defaultValue = "/") String link,
-			Model model) {
-		return model.addAttribute("link", link);
+	
+	@RequestMapping(value = "impressum")
+	public String impressum() {
+		return "home/impressum";
 	}
 }
