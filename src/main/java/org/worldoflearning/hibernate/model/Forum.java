41670f4e53a2f5c4223d8087d07f07ebdbdb0b12
trini148 <elias.mr1@gmail.com>
2015-07-08 08:48:16 +0200

    Attributnamen
    in Forum aktualisiert

--- src/main/java/org/worldoflearning/hibernate/model/Forum.java
+++ src/main/java/org/worldoflearning/hibernate/model/Forum.java
@@ -20,24 +20,24 @@ public class Forum {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID", nullable = false, unique = true, length = 11)
-	private int id;
+	private int forum_id;
 
 	@Column(name = "FORUM_NAME", length = 20, nullable = true)
 	private String forum_name;
 
-	public int getId() {
-		return id;
+	public int getForum_Id() {
+		return forum_id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public void setForum_Id(int forum_id) {
+		this.forum_id = forum_id;
 	}
 
-	public String getName() {
+	public String getForum_Name() {
 		return forum_name;
 	}
 
-	public void setName(String forum_name) {
+	public void setForum_Name(String forum_name) {
 		this.forum_name = forum_name;
 	}
 
