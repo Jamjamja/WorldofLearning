36aec2c817d6d90fbc89e1af694c00dd2746b23f
trini148 <elias.mr1@gmail.com>
2015-07-12 15:26:39 +0200

    Gamifications-Klassen hinzugefügt

---
+++ src/main/java/org/worldoflearning/hibernate/model/Achievement.java
@@ -0,0 +1,5 @@
+package org.worldoflearning.hibernate.model;
+
+public class Achievement {
+
+}
---
+++ src/main/java/org/worldoflearning/hibernate/model/Quest.java
@@ -0,0 +1,60 @@
+package org.worldoflearning.hibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/*
+ * Nur Getter und Setter sind bereits implementiert. Die anderen Methoden müssen noch implementiert werden.
+ */
+@Entity
+@Table(name = "Quest", uniqueConstraints = { @UniqueConstraint(columnNames = { "QUEST_NAME" }) })
+public class Quest {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "QUEST_NAME", length = 20, nullable = true)
+	private String quest_name;
+	
+	@Column(name = "QUEST_BESCHREIBUNG", length = 20, nullable = true)
+	private String quest_beschreibung;
+	
+	@Column(name = "ANZAHL_PUNKTE", nullable = false, unique = true, length = 11)
+	private int anzahl_punkte;
+	
+	/*
+	 * Getter und Setter
+	 */
+	
+	public String getQuest_name() {
+		return quest_name;
+	}
+	public void setQuest_name(String quest_name) {
+		this.quest_name = quest_name;
+	}
+	public String getQuest_beschreibung() {
+		return quest_beschreibung;
+	}
+	public void setQuest_beschreibung(String quest_beschreibung) {
+		this.quest_beschreibung = quest_beschreibung;
+	}
+	public int getAnzahl_punkte() {
+		return anzahl_punkte;
+	}
+	public void setAnzahl_punkte(int anzahl_punkte) {
+		this.anzahl_punkte = anzahl_punkte;
+	}
+
+	/*
+	 * Methoden
+	 */
+	
+	public void erfülleQuest(int anzahl_punkte, int benutzer_anzahl_punkte){
+		
+	}
+}
+
